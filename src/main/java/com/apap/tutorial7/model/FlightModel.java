ce85cbe62dc0f5c1bf9599835af2dc598340c296
Teresa Prima <teresaprima98@gmail.com>
2018-11-01 23:37:34 +0700

    fix flight

--- src/main/java/com/apap/tutorial7/model/FlightModel.java
+++ src/main/java/com/apap/tutorial7/model/FlightModel.java
@@ -14,7 +14,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
