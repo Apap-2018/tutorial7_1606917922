312baeb38952a997d1f060a04e6d19bbe9368a0a
Teresa Prima <teresaprima98@gmail.com>
2018-11-01 20:33:39 +0700

    first commit

--- src/main/java/com/apap/tutorial7/rest/Setting.java
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -4,3 +4,4 @@ public class Setting {
 	final public static String pilotUrl = "https://f497e9c3-998f-45f7-8306-98402f0ba3b5.mock.pstmn.io";
 	final public static String airports = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=T9hDZRakSuaAfBZpLzr8G8CgtSU4x9IK&term=";
 }
+
